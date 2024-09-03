df1aeac99e2018b2ada7b2fcdf734bb1e9388caa
Bruno Galvão Martins <bruno.martins@client-craft.com>
2024-09-03 18:02:41 -0300

    feat: add new template for archive product, add functions to call products for each category, create styels for archive product and product component, move components from file component for a folder with same name

--- src/functions_inc/formatProducts.php
+++ src/functions_inc/formatProducts.php
@@ -1,7 +1,59 @@
 <?php
-function formar_products($products, $img_size = 'medium') {
+function format_products($products, $img_size = 'medium') {
   $products_final = [];
   foreach($products as $product) {
-    if()
+    if( $product ->is_type('variable')) {
+      $min_price = wc_price($product->get_variation_regular_price('min', true));
+      $max_price = wc_price($product->get_variation_regular_price('max', true));
+      $product_id = $product->get_id();
+    } else {
+      $regular_price = $product->get_regular_price();
+      $product_id = $product->get_id();
+    }
+    $products_final[] = [
+      'name' => $product->get_name(),
+      'link' => $product->get_permalink(),
+      'max-price' => $max_price,
+      'min-price'=> $min_price,
+      'img' => wp_get_attachment_image_src($product->get_image_id(), $img_size)[0],
+      'id' => $product_id
+    ];
   }
+  return $products_final;
 }
+?>
+<?php
+function barelaser_product_list($products) {
+  $img_url = get_stylesheet_directory_uri() . '/dist/imgs';
+?>
+<nav id="category__product-container">
+  <ul class="category__product-list">
+    <?php foreach($products as $product) {?>
+  <li class="category__product-thumb">
+  <img src="<?= $product['img'] ?>" alt="<?= $product['name'] ?>">
+  <div class="category__product-thumb__content">
+    <h3><?= $product['name'] ?></h3>
+    <div class="product__price">
+      <p><?= $product['min-price'] . ' - ' . $product['max-price'] ?></p>
+    </div>
+    <div class="afterpay__info">
+      <?php echo do_shortcode(('[afterpay_paragraph]')); ?>
+    </div>
+    <?php 
+    $wc_product = wc_get_product( $product['id']);
+    if($wc_product->is_type('variable')) {?>
+      <a href="<?= $product['link'] ?>" class="cta-btn product__cta">select options</a>
+    <?php } else { ?>
+      <a href="<?= esc_url( '?add-to-cart=' . $product['id'] ); ?>" 
+                 class="add_to_cart_button ajax_add_to_cart cta-btn product__cta" 
+                 data-product_id="<?= $product['id'] ?>" 
+                 data-quantity="1" 
+                 aria-label="<?= esc_attr( $product['name'] ) ?>" 
+                 rel="nofollow">Add to Cart</a>
+    <?php }?>
+    </div>
+    </li>
+    <?php }?>
+  </ul>
+</nav>
+<?php }?>
--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -4,7 +4,57 @@ $products=[];
 if (have_posts()) {while(have_posts()) {the_post();
 $products[] = wc_get_product(get_the_ID());
 } };
-// $data['products'] = format_products($products);
+ $data['products'] = format_products($products);
+  $current_cat = get_queried_object();
+  $current_cat_id = get_queried_object_id();
+
+  $sess1__ctas = get_field('cta_buttons', 'term_'. $current_cat_id);
+  $sess2__img = get_field('sess2_image', 'term_'. $current_cat_id);
+  $sess2__head = get_field('sess2_headline', 'term_'. $current_cat_id);
+  $sess2__text = get_field('sess2_content_text', 'term_'. $current_cat_id);
+  $sess2__cta = get_field('sess2_cta_label', 'term_'. $current_cat_id);
 ?>
-<img src="<?php the_field('category_image') ?>" alt="">
+<main>
+  <section id="sess1__archive">
+    <img src="<?= the_field('sess1_image_bg_desk', 'term_'. $current_cat_id) ?>" alt="<?= esc_html($current_cat->name) ?>" class="sess1__archive-bg-desk">
+    <img src="<?= the_field('sess1_image_bg_mobile', 'term_'. $current_cat_id) ?>" alt="<?= esc_html($current_cat->name) ?>" class="sess1__archive-bg-mobile">
+    <div class="sess1__archive-container">
+      <h1><?= esc_html($current_cat->name) ?></h1>
+      <p class="sess1__archive-subtitle">
+        <?= the_field('sess1_subtitle', 'term_'. $current_cat_id) ?>
+      </p>
+      <div class="sess1__archive-ctas__wrapper">
+      <?php foreach($sess1__ctas as $cta) { ?>
+      <a href="<?= $cta['cta_link'] ?>" class="cta-btn sess1__archive-cta">
+        <?= $cta['cta_label'] ?>
+      </a>
+      <?php } ?>
+      </div>
+    </div>
+  </section>
+  <?php if($sess2__img && $sess2__text) { ?>
+  <section id="sess2__archive">
+    <div class="sess2__archive-container">
+      <img src="<?= $sess2__img ?>" alt="">
+      <div class="sess2__archive-content">
+        <?php if($sess2__head) { ?>
+        <h2><?= $sess2__head ?></h2>
+        <?php } ?>
+        <div class="sess2_archive-content__text">
+        <?= $sess2__text ?>
+        </div>
+        <?php if($sess2__cta) { ?>
+        <a href="<?= the_field('sess2_cta_link', 'term_'. $current_cat_id) ?>" class="cta-btn sess2__archive-cta"><?= $sess2__cta ?></a>
+        <?php } ?>
+      </div>
+    </div>
+  </section>
+  <?php } ?>
+  <section id="sess3__archive">
+    <div class="sess3__archive-content">
+      <?php barelaser_product_list($data['products']) ?>
+    </div>
+  </section>
+</main>
+
 <?php get_footer() ?>
